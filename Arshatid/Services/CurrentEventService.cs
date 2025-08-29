0576f3a6f811eac4313ae6acd53ba00793752ae3
Tolli <tolli@kopavogur.is>
2025-08-29 20:47:05 +0000

    Add IslandIs registration API and core services

---
+++ Arshatid/Services/CurrentEventService.cs
@@ -0,0 +1,23 @@
+using Arshatid.Databases;
+using ArshatidModels.Models.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Arshatid.Services;
+
+public class CurrentEventService
+{
+    private readonly ArshatidDbContext _dbContext;
+
+    public CurrentEventService(ArshatidDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public ArshatidEvent? GetCurrentEvent(DateTime nowLocal)
+    {
+        return _dbContext.ArshatidEvents
+            .Where(e => e.RegistrationStartTime <= nowLocal && nowLocal <= e.RegistrationEndTime)
+            .OrderByDescending(e => e.RegistrationStartTime)
+            .FirstOrDefault();
+    }
+}
