3b411aab357d6015a697abad67ac268443c4c03f
Tolli <tolli@kopavogur.is>
2025-08-29 17:24:49 +0000

    Add EF models and configure contexts

---
+++ ArshatidModels/Models/EF/ArshatidImage.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArshatidModels.Models.EF;
+
+[Table("ArshatidImage", Schema = "dbo")]
+public class ArshatidImage
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Pk { get; set; }
+
+    [Column("ArshatifFk")]
+    public int ArshatidFk { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string ContentType { get; set; } = string.Empty;
+
+    [Required]
+    public byte[] ImageData { get; set; } = [];
+
+    public int ImageTypeFk { get; set; }
+
+    [ForeignKey(nameof(ArshatidFk))]
+    public virtual ArshatidEvent ArshatidEvent { get; set; } = null!;
+
+    [ForeignKey(nameof(ImageTypeFk))]
+    public virtual ArshatidImageType ImageType { get; set; } = null!;
+}
