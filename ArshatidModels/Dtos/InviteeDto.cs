cd5930d2aa1292b8f0fef1b1e0da851032582a60
Þorvaldur Sigurður Arnarsson <tolli@kopavogur.is>
2025-09-01 09:49:43 +0000

    commit

--- ArshatidModels/Dtos/InviteeDto.cs
+++ ArshatidModels/Dtos/InviteeDto.cs
@@ -3,6 +3,7 @@ namespace ArshatidModels.Dtos;
 public sealed class InviteeDto
 {
     public int InviteeId { get; set; }
+    public int EventId { get; set; }
     public string Ssn { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
 }
