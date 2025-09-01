350655dd70e0b94a86598d108228b476542797f4
Tolli <tolli@kopavogur.is>
2025-09-01 19:36:24 +0000

    Replace Svið dropdown with searchable cost center list

--- ArshatidPublic/Models/RegistrationViewModel.cs
+++ ArshatidPublic/Models/RegistrationViewModel.cs
@@ -8,8 +8,6 @@ public class RegistrationViewModel
     public bool Plus { get; set; }
     [Display(Name = "Fæðuóþol")]
     public string? Alergies { get; set; }
-    [Display(Name = "Svið")]
-    public string? OrgUnitName { get; set; }
     [Display(Name = "Kostnaðarstaður")]
     public int? ArshatidCostCenterFk { get; set; }
     [Display(Name = "Vegan")]
