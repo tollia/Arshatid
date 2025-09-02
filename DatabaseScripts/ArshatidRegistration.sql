2b6031f98ab414b99559aef26ba40a0053533390
Tolli <tolli@kopavogur.is>
2025-09-02 13:21:25 +0000

    Add gender field to invitee

--- DatabaseScripts/ArshatidRegistration.sql
+++ DatabaseScripts/ArshatidRegistration.sql
@@ -1,13 +1,15 @@
 CREATE  TABLE TolliTest.dbo.ArshatidRegistration ( 
 	Pk                   int    IDENTITY ( 1 , 1 )  NOT NULL,
 	Plus                 int  DEFAULT 0    NOT NULL,
-	Alergies             nvarchar(255)      NULL,
-	ArshatidInviteeFk    int      NOT NULL,
-	Vegan                bit  DEFAULT 0    NOT NULL,
-	CONSTRAINT pk_ArshatidRegistrations PRIMARY KEY  ( Pk ) ,
-	CONSTRAINT unq_ArshatidRegistration_Invitee UNIQUE ( ArshatidInviteeFk ) 
- );
+        Alergies             nvarchar(255)      NULL,
+        ArshatidInviteeFk    int      NOT NULL,
+        ArshatidCostCenterFk int      NULL,
+        Vegan                bit  DEFAULT 0    NOT NULL,
+        CONSTRAINT pk_ArshatidRegistrations PRIMARY KEY  ( Pk ) ,
+        CONSTRAINT unq_ArshatidRegistration_Invitee UNIQUE ( ArshatidInviteeFk )
+);
 
 ALTER TABLE TolliTest.dbo.ArshatidRegistration ADD CONSTRAINT fk_arshatidregistration FOREIGN KEY ( ArshatidInviteeFk ) REFERENCES TolliTest.dbo.ArshatidInvitee( pk );
+ALTER TABLE TolliTest.dbo.ArshatidRegistration ADD CONSTRAINT fk_arshatidregistration_costcenter FOREIGN KEY ( ArshatidCostCenterFk ) REFERENCES TolliTest.dbo.ArshatidCostCenter( Pk );
 GO
 
